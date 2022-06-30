1ed3b4f6cb1485bcc95016ad1afcefcf43263b18
Muhammad Tanveer <muhammadtanveer.bcsf18@iba-suk.edu.pk>
2022-06-30 20:54:00 +0500

    storing and retrieving categories from firestore

--- src/context/ProductsContext.jsx
+++ src/context/ProductsContext.jsx
@@ -1,13 +1,22 @@
-import { createContext,useState } from "react";
+import { createContext,useEffect,useState } from "react";
 
-import PRODUCTS from '../data/shop-data.json'
+import SHOP_DATA from '../data/shop-data.js'
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
 
 export const ProductsContext = createContext({
     products:[],
 });
 
 export const ProductsProvider = ({children}) => {
-    const [products, setProducts] = useState(PRODUCTS)
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        const getCategoriesMap = async () => {
+            const categoryMap = await getCategoriesAndDocuments();
+            console.log(categoryMap);
+        }
+        getCategoriesMap();
+    }, [])
+
     const value = {products}
 
     return(
