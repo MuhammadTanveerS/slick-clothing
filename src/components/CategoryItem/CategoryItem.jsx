4ba50c504f4bfc8b64809f80fcbca268684f17b2
Muhammad Tanveer <muhammadtanveer.bcsf18@iba-suk.edu.pk>
2022-07-02 20:56:34 +0500

    finished styling categories

--- src/components/CategoryItem/CategoryItem.jsx
+++ src/components/CategoryItem/CategoryItem.jsx
@@ -4,9 +4,9 @@ import './categoryitem.styles.scss'
 const CategoryItem = ({category}) => {
     const {title,imageUrl} = category;
   return (
-    <div className="category-container">
+    <div className="category-details-container">
           <div className="bg-Image" style={{backgroundImage:`url(${imageUrl})`}}/>
-          <div className="category-body">
+          <div className="category-details-body">
             <h2>{title}</h2>
             <p>Shop Now</p>
           </div>
